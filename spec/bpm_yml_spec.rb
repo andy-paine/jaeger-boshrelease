a167e5c8d2affc8ee960a1aa50f2801c4dc2e03a
Andy Paine <andy.paine@engineerbetter.com>
2020-04-29 22:23:14 +0100

    Add collector options
    * Simple values are passed straight through to their command line flags
    * List and maps are formatted to item1,item2 or key1=value1,key2=value2
    * TLS is only enabled when a key and cert are provided
    * Job properties take content of key/cert rather than location and write
    to templates at well known location

--- spec/bpm_yml_spec.rb
+++ spec/bpm_yml_spec.rb
@@ -26,10 +26,58 @@ describe 'jaeger-all-in-one bpm.yml' do
     end
 
     it 'sets badger CLI flags' do
-      process = get_process_from_bpm(YAML::load template.render(config))
-      expect(process['args']).to include '--badger.ephemeral=false'
-      expect(process['args']).to include '--badger.directory-key=/var/vcap/store/jaeger-all-in-one/keys'
-      expect(process['args']).to include '--badger.directory-value=/var/vcap/store/jaeger-all-in-one/values'
+      args = get_process_from_bpm(YAML::load template.render(config))['args']
+      expect(args).to include '--badger.ephemeral=false'
+      expect(args).to include '--badger.directory-key=/var/vcap/store/jaeger-all-in-one/keys'
+      expect(args).to include '--badger.directory-value=/var/vcap/store/jaeger-all-in-one/values'
+    end
+  end
+
+  describe 'collector configuration' do
+    it 'should set sensible defaults for collector configuration' do
+      args = get_process_from_bpm(YAML::load template.render({}))['args']
+      expect(args).to include '--collector.grpc-port=14250'
+      expect(args).to include '--collector.grpc.tls.enabled=false'
+      expect(args).to include '--collector.http-port=14268'
+      expect(args).to include '--collector.num-workers=50'
+      expect(args).to include '--collector.port=14267'
+      expect(args).to include '--collector.queue-size=2000'
+      expect(args).to include '--collector.queue-size-memory=0'
+      expect(args).to include '--collector.zipkin.http-port=0'
+      expect(args).to include "--collector.zipkin.allowed-headers='content-type'"
+      expect(args).to include "--collector.zipkin.allowed-origins='*'"
+
+      expect(args).to_not include '--collector.tags'
+      expect(args).to_not include '--collector.grpc.tls.cert'
+      expect(args).to_not include '--collector.grpc.tls.key'
+      expect(args).to_not include '--collector.grpc.tls.client-ca'
+    end
+
+    it 'should enable grpc tls when a key and certificate are provided' do
+      config = { 'collector' => { 'grpc' => { 'tls' => { 'private_key' => '--- BEGIN KEY ---', 'certificate' => '--- BEGIN CERT ---' }}}}
+      args = get_process_from_bpm(YAML::load template.render(config))['args']
+      expect(args).to include '--collector.grpc.tls.enabled=true'
+      expect(args).to include '--collector.grpc.tls.cert=/var/vcap/jobs/jaeger-all-in-one/tls/grpc.crt'
+      expect(args).to include '--collector.grpc.tls.key=/var/vcap/jobs/jaeger-all-in-one/tls/grpc.key'
+    end
+
+    it 'should include grpc client-ca cert when provided' do
+      config = { 'collector' => { 'grpc' => { 'tls' => { 'client-ca' => '--- BEGIN CERT ---' }}}}
+      args = get_process_from_bpm(YAML::load template.render(config))['args']
+      expect(args).to include '--collector.grpc.tls.client-ca=/var/vcap/jobs/jaeger-all-in-one/tls/grpc-client-ca.crt'
+    end
+
+    it 'should include tags as a comma separated list of key=value' do
+      config = { 'collector' => { 'tags' => { 'key1' => 'value1', 'key2' => 'value2' }}}
+      args = get_process_from_bpm(YAML::load template.render(config))['args']
+      expect(args).to include "--collector.tags='key1=value1,key2=value2'"
+    end
+
+    it 'should provide zipkin options as comma separated lists' do
+      config = { 'collector' => { 'zipkin' => { 'allowed-headers' => ['header1', 'header2'], 'allowed-origins' => ['origin1', 'origin2'] }}}
+      args = get_process_from_bpm(YAML::load template.render(config))['args']
+      expect(args).to include "--collector.zipkin.allowed-headers='header1,header2'"
+      expect(args).to include "--collector.zipkin.allowed-origins='origin1,origin2'"
     end
   end
 end
