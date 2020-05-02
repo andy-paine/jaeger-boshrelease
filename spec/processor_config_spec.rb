54c1c8a460f061dd5b59c9249a41d96709dad008
Andy Paine <andy.paine@engineerbetter.com>
2020-05-02 13:13:45 +0100

    Switch from "server-host-port" -> "port"
    Wherever possible, this release uses the exact same property names as
    flag names from Jaeger. In situations like this though, BOSH has the
    concept of links for supplying host addresses so it makes more sense to
    only define the port from an operators perspective. This will mean that
    link consumes can just do:
    `link('jaeger-agent').address:link('jaeger-agent').p('processor.jaeger-binary.port')`
    which is a much more typical workflow for BOSH operators.

--- spec/processor_config_spec.rb
+++ spec/processor_config_spec.rb
@@ -11,15 +11,15 @@ require_relative 'spec_helper.rb'
 
     it 'should set sensible defaults' do
       args = get_process_from_bpm(YAML::load(template.render({})), job_name)['args']
-      expect(args).to include '--processor.jaeger-binary.server-host-port=:6832'
+      expect(args).to include '--processor.jaeger-binary.server-host-port=0.0.0.0:6832'
       expect(args).to include '--processor.jaeger-binary.server-max-packet-size=65000'
       expect(args).to include '--processor.jaeger-binary.server-queue-size=1000'
       expect(args).to include '--processor.jaeger-binary.workers=10'
-      expect(args).to include '--processor.jaeger-compact.server-host-port=:6831'
+      expect(args).to include '--processor.jaeger-compact.server-host-port=0.0.0.0:6831'
       expect(args).to include '--processor.jaeger-compact.server-max-packet-size=65000'
       expect(args).to include '--processor.jaeger-compact.server-queue-size=1000'
       expect(args).to include '--processor.jaeger-compact.workers=10'
-      expect(args).to include '--processor.zipkin-compact.server-host-port=:5775'
+      expect(args).to include '--processor.zipkin-compact.server-host-port=0.0.0.0:5775'
       expect(args).to include '--processor.zipkin-compact.server-max-packet-size=65000'
       expect(args).to include '--processor.zipkin-compact.server-queue-size=1000'
       expect(args).to include '--processor.zipkin-compact.workers=10'
